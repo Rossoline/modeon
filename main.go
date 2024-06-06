bf54f462c5385ec603eac8bc9cc6754bd5c6c978
Ilja Nekrituk <Nekrit99@gmail.com>
2024-06-06 23:03:30 +0300

    Add Person, info

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	Name        string
+	PhoneNumber string
+}
+
+//Task Manager App
+
+func main() {
+	person1 := Person{"New person", "101"}
+	info(person1)
+}
+
+func info(p Person) {
+	fmt.Println(p)
 }
