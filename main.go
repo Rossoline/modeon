b25b61318217c5afacc46967ab82f28c53bdd0e9
Ilja Nekrituk <Nekrit99@gmail.com>
2024-06-11 23:11:47 +0300

    modified Task

--- main.go
+++ main.go
@@ -8,22 +8,28 @@ import (
 type Person struct {
 	Name        string
 	PhoneNumber string
-	Task        []string
+	Task        []Task
+}
+
+type Task struct {
+	Date        string
+	Description string
 }
 
 //Task Manager App
 
 func main() {
-
+	p := Person{}
+	info(p)
 }
 
 func info(p Person) {
 	fmt.Println(p)
 }
 
-func AddTask(p *Person, s string) {
-	if slices.Contains(p.Task, s) {
+func AddTask(p *Person, t Task) {
+	if slices.Contains(p.Task, t) {
 		return
 	}
-	p.Task = append(p.Task, s)
+	p.Task = append(p.Task, t)
 }
